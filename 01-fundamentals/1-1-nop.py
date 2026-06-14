487e493d60525a6939ebe0a0f8ac31d9089d2e0c
Fumio <foobarbazfred@gmail.com>
2026-06-14 17:29:41 +0900

    Create 1-1-nop.py

---
+++ 01-fundamentals/1-1-nop.py
@@ -0,0 +1,29 @@
+#
+#
+# asm_pio .... assemble and make code of PIO program
+# only NOP
+# sample program ID:  1-1
+
+#
+
+import machine
+from machine import Pin
+import rp2
+
+@rp2.asm_pio()
+def sm_inst_nop():
+    nop()
+
+STATE_MACHINE_ID=0
+
+machine.freq(125_000_000)  # set 125MHz to same RP2040
+
+# create Statemachine and code is nop
+sm0 = rp2.StateMachine(STATE_MACHINE_ID, sm_inst_nop, freq=2000)
+
+# start StateMachine
+sm0.active(1)
+
+#
+#
+#
